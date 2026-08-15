4c1f802d10b353f45d69ea13e19f0829912d0774
Vladimir <milliganb43@gmail.com>
2026-08-15 19:27:28 +0300

    Create class RegistrationUserDTO

---
+++ src/main/java/by/tms/ecommerceprojectc41onl/dto/RegistrationUserDTO.java
@@ -0,0 +1,29 @@
+package by.tms.ecommerceprojectc41onl.dto;
+
+import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.HibernateValidatorConfiguration;
+
+
+
+
+public class RegistrationUserDTO {
+@NotBlank
+@NotEmpty
+@Size(min = 8, max = 16)
+ private String name;
+ @NotBlank
+ @NotEmpty
+ @Size(min = 8, max = 16)
+ private String lastname;
+ @Pattern(regexp = "[a-zA-Z0-9@#%&*]{8,20},message = \"Эмодзи не разрешены\"")
+ private String password;
+ @Size(min = 8, max = 20)
+ private String email;
+ @Pattern(regexp = "^[0-9+]{7,15}$", message = "Некорректный формат телефона")
+ private long phone;
+}
+
