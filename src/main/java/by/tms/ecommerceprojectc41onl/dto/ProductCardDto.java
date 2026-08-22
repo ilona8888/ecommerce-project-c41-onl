95a644c799287ba7f985020cb349178d9495e4bc
IrinaMizgir <ira.mizgir797@gmail.com>
2026-08-23 00:37:52 +0300

    TMS-73 | Добавила карточку товара.

---
+++ src/main/java/by/tms/ecommerceprojectc41onl/dto/ProductCardDto.java
@@ -0,0 +1,23 @@
+package by.tms.ecommerceprojectc41onl.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+/**
+ * DTO для карточки товара.
+ *
+ * @author Ирина Мизгир
+ * @date 22.08.2026 23:32
+ */
+@Data
+public class ProductCardDto {
+
+    private final long id;
+    private final String name;
+    private final BigDecimal price;
+    private final String description;
+    private final Long photoId;
+    private final double score;
+    private final boolean favourite;
+}
